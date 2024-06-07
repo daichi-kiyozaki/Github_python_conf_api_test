d75bb9f6fed5c5633a6c2ca9b8faf3d21b0b5dfd
kana-sakuma <kana_sakuma@nuco.co.jp>
2024-06-07 19:04:29 +0900

    conf Python API test

--- confluence_api_test.py
+++ confluence_api_test.py
@@ -4,23 +4,79 @@ import config
 confluence = Confluence(url=config.CONFLUENCE_URL, username=config.CONFLUENCE_USERNAME, password=config.CONFLUENCE_TOKEN)
 
 # idでページ情報を取得
-page_id = "11993089"
+page_id = "7340033"
 res = confluence.get_page_by_id(
     page_id=page_id,
-    expand="space,body.view"  # スペース情報やページの本文を取得
+    expand="space,body.storage"  # スペース情報やページの本文を取得
 )
-
-print(res["space"]["key"])
-print(res["id"])
-print(res["title"])
-print(res["body"]["view"]["value"])  # 本文 (HTML) の先頭 100 文字
-
+print("---res---")
+print(res)
+'''
 # タイトルでページ情報を取得
 page_title = "conv_check_test_2"
 res2 = confluence.get_page_by_title(
     space=config.CONFLUENCE_SPACE_KEY,
     title=page_title
 )
-
+print("---res2---")
 print(res2)
+'''
+# ページが存在する場合、本文をbodyの内容で更新
+page_id = "15007859"
+title = "更新時に書式が反映されるかのテスト_更新後"
+body = res["body"]["storage"]["value"]
+update_res = confluence.update_page(
+    page_id=page_id,
+    title=title,
+    body=body,
+    full_width=True
+)
+print("---update_res---")
+print(update_res)
+'''
+# タイトル指定でページ内容を更新（指定したページがない場合は新規作成）、必ず子ページになる
+parent_id = "15040513"
+title = "API_update_test1_6/7_更新後2_width-test"
+body = "更新後2_width-test"
+update_res2 = confluence.update_or_create(
+    parent_id=parent_id,
+    title=title,
+    body=body,
+    full_width=True
+)
+print("---update_res2---")
+print(update_res2)
 
+# 添付ファイルをアップロードしたい場合
+page_id = "15040573"
+filename = "images/boy01_laugh.png"
+upload_res = confluence.attach_file(
+    filename=filename,
+    page_id=page_id
+)
+print("---upload_res---")
+print(upload_res)
+'''
+'''
+# 添付コンテンツをアップロードしたい場合
+page_id = "15040595"
+content = "images/boy01_laugh.png"
+upload_res2 = confluence.attach_content(
+    content=content,
+    name="irasutoya_boy",
+    page_id=page_id
+)
+print("---upload_res2---")
+print(upload_res2)
+'''
+
+# ページを移動させたい場合
+page_id = "11993089"
+parent_title = "readme.md"
+move_res = confluence.move_page(
+    space_key=config.CONFLUENCE_SPACE_KEY,
+    page_id=page_id,
+    target_title=parent_title
+)
+print("---move_res---")
+print(move_res)
